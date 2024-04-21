6ec4590929b1b187942f503519d4abe3515c8876
luqitas2 <lrh8798@nyu.edu>
2024-04-21 12:25:57 -0400

    Update Final_V1_LIGHTSKETCH.ino
    added millis
    made first version of verse animation

--- Final_V1_LIGHTSKETCH.ino
+++ Final_V1_LIGHTSKETCH.ino
@@ -1,9 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_PIN 32
-#define LED_COUNT 56
+#define LED_COUNT 16
 
-Adafruit_NeoPixel strip(56, 32, NEO_GRB);  //panel 1
+Adafruit_NeoPixel strip(16, 32, NEO_GRB);  //panel 1
 
 uint32_t lightBlue = strip.Color(60, 127, 255);
 uint32_t softYellow = strip.Color(255, 164, 0);
@@ -16,21 +16,27 @@ long randNumber1;
 long randNumber2;
 long randNumber3;
 
+unsigned long lastStepTime = 0;
+int stepLength = 1000;
+int currentStep = 0;
+int totalSteps = 6;
+
+unsigned long lastStepTime2 = 0;
+int stepLength2 = 1000;
+
 void setup() {
 
   randomSeed(digitalRead(0));
 
   strip.begin();
   strip.clear();
-  strip.setBrightness(80);
+  strip.setBrightness(80);  //eventually this will be variable with slider/expression pedal
   strip.show();
 }
 
 void loop() {
   strip.clear();
-  // // stripPulseOrange(25);
-  // strip.fill(orange);
-  sparkleOverFill(25);
+  verseSatellite();
   strip.show();
 
   randNumber = random(56);
@@ -41,6 +47,17 @@ void loop() {
 
 
 void verseSatellite() {
+  strip.clear();
+  if (millis() > lastStepTime + stepLength) {
+    lastStepTime = millis();
+    strip.fill(orange);
+    nextStep();
+  }
+  if(millis() > lastStepTime2 + stepLength2){
+    lastStepTime2 = millis();
+    sparkleOverFill(25);
+    nextStep();
+  }
 }
 
 void verse2Satellite() {
@@ -130,5 +147,35 @@ void sparkleOverFill(int wait) {
     strip.setPixelColor(randNumber3, (i * 0.6), (i * 1.27), (i * 2.55));
     delay(wait);
     strip.show();
+  }
+}
+
+
+
+void stripPulseOrange_Millis(int wait) {
+  for (int i = 100; i > 0; i--) {
+    strip.setPixelColor(0, (i * 2.55), i, 0);
+    strip.setPixelColor(1, (i * 2.55), i, 0);
+    strip.setPixelColor(2, (i * 2.55), i, 0);
+    strip.setPixelColor(3, (i * 2.55), i, 0);
+    delay(wait);  //would have to make this a variable step length based on the song tempo
+    strip.show();
+    if (i == 1) {
+      for (int j = 1; j < 100; j++) {
+        strip.setPixelColor(0, (j * 2.55), j, 0);
+        strip.setPixelColor(1, (j * 2.55), j, 0);
+        strip.setPixelColor(2, (j * 2.55), j, 0);
+        strip.setPixelColor(3, (j * 2.55), j, 0);
+        delay(wait);
+        strip.show();
+      }
+    }
+  }
+}
+
+void nextStep() {
+  currentStep = currentStep + 1;
+  if (currentStep >= totalSteps) {
+    currentStep = 0;
   }
 }
